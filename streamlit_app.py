109fe08f96300061821364c315a17c40ef241680
Patricia Sotelo <120718441+psotelog@users.noreply.github.com>
2022-12-16 13:31:48 +0100

    Update streamlit_app.py
    Modificamos el índice de selección para el usuario para que no sea por id si no por el nombre de  la fruta

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,15 @@ streamlit.text ('🐔 Hard-Boiledd Free-Range Egg')
 streamlit.text ('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# Añadimos la librería pandas para poder importarun fichero del bucket y lo vamos a visualizar como una tabla mediante un DataFrame
+# Añadimos la librería pandas para poder importar un fichero del bucket para poder visualizarlo  luego mediante un DataFrame
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe (my_fruit_list)
+
+#Esto nos permite que el índice de la  tabla no vaya por id, si no por el campo Fruit (Nombre)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Incluimos una lista que me permita seleccionar los registros que quiera 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Visualizamos el dataframe con los datos del fichero
+streamlit.dataframe (my_fruit_list)
