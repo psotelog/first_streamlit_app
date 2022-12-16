fcaf0066767e76ea595dc7843cdcde3a133cffb1
Patricia Sotelo <120718441+psotelog@users.noreply.github.com>
2022-12-16 13:18:12 +0100

    Update streamlit_app.py
    Added emojis, avocado and fruit smoothie to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast  Menu')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, Spinach & Rocket Smoothie')
-streamlit.text ('Hard-Boiledd Free-Range Egg')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text ('🐔 Hard-Boiledd Free-Range Egg')
+streanlit.text ('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
