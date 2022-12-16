259052bac3a975f13688cf6882d5b41fce32d46c
Patricia Sotelo <120718441+psotelog@users.noreply.github.com>
2022-12-16 13:25:53 +0100

    Update streamlit_app.py
    Añadir un lista de seleccion

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,10 @@ streamlit.text ('🐔 Hard-Boiledd Free-Range Egg')
 streamlit.text ('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
+# Añadimos la librería pandas para poder importarun fichero del bucket y lo vamos a visualizar como una tabla mediante un DataFrame
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe (my_fruit_list)
+
+# Incluimos una lista que me permita seleccionar los registros que quiera 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
