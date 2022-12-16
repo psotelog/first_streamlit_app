93be26896d0efcfef095a962fa3ebd7e2ac9da0d
Patricia Sotelo <120718441+psotelog@users.noreply.github.com>
2022-12-16 13:13:58 +0100

    Update streamlit_app.py
    New lines for the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title ('My Parents New Healthy Diner')
-streamlit.title ('Breakfast  Menu')
-streamlit ('Omega 3 & Blueberry Oatmeal')
-streamlit ('Kale, Spinach & Rocket Smoothie')
-streamlit ('Hard-Boiledd Free-Range Egg')
+streamlit.header ('Breakfast  Menu')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
+streamlit.text ('Hard-Boiledd Free-Range Egg')
