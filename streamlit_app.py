798e7333d7a5d5ac7d3975c095fd8fe4e0a6776c
Patricia Sotelo <120718441+psotelog@users.noreply.github.com>
2022-12-19 18:00:21 +0100

    Update streamlit_app.py
    quitamos que aparezca el json en la pantalla

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #solo muestra los datos en la pantalla
+#streamlit.text(fruityvice_response.json())  #solo muestra los datos en la pantalla
 
 #cogemos el json de la respuesta y lo normalizamos
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
