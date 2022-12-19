93c39af0bfee6d4b64b25e429bf19b063f191e53
Patricia Sotelo <120718441+psotelog@users.noreply.github.com>
2022-12-19 21:10:07 +0100

    añadimos segunda caja

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,3 +59,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+# Nueva sección para tener una segunda caja de texto para  entrar datos
+streamlit.header('Fruityvice Fruit Advice!')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('The user entered ', add_my_fruit)
